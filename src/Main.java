fda12c5e2c9a508277ab5cfe5f9a74dcb024a19b
mafuteru <linkpichucheez@gmail.com>
2017-05-11 14:18:23 -0700

    1st Upate

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+/**
+ * Created by lij7451 on 5/11/2017.
+ */
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    Button button;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Title of the Window");
+        button = new Button("Click me");
+
+        button.setOnAction(e -> {
+            System.out.println("Lambda expressions are awesome!");
+            System.out.println("hey now brown cow");
+        });
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+        Scene scene = new Scene(layout, 300, 250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
