0346df89f6701fc8815da5b79e61001363b7e632
mafuteru <linkpichucheez@gmail.com>
2017-05-19 13:25:57 -0700

    i cry

--- src/TextTest.java
+++ src/TextTest.java
@@ -4,13 +4,14 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.text.*;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 public class TextTest extends Application {
 
     Stage window;
-    Scene scene;
+    Scene scene1, scene2;
     Button button;
 
     public static void main(String[] args) {
@@ -26,20 +27,28 @@ public class TextTest extends Application {
 
         //Form
         TextField nameInput = new TextField();
+        String name = nameInput.getText();
 
         button = new Button("Submit");
         button.setOnAction( e -> {
-            System.out.println(nameInput.getText());
-            System.out.println("!");
+            window.setScene(scene2);
         } );
 
         //Layout
-        VBox layout = new VBox(10);
-        layout.setPadding(new Insets(20, 20, 20, 20));
-        layout.getChildren().addAll(label1, nameInput, button);
+        VBox layout1 = new VBox(10);
+        layout1.setPadding(new Insets(20, 20, 20, 20));
+        layout1.getChildren().addAll(label1, nameInput, button);
 
-        scene = new Scene(layout, 300, 250);
-        window.setScene(scene);
+        //Label label2 = new Label("Hello, "+nameInput.getText());
+        Text text = new Text("Hello there "+name);
+
+        VBox layout2 = new VBox(20);
+        layout2.setPadding(new Insets(20, 20, 20, 20));
+        layout2.getChildren().addAll(text);
+        scene2 = new Scene(layout2, 200, 200);
+
+        scene1 = new Scene(layout1, 300, 250);
+        window.setScene(scene1);
         window.show();
     }
 
