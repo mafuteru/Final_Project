7b6458f0d9259214ded1c1c018ae50c1071eb256
mafuteru <linkpichucheez@gmail.com>
2017-05-30 14:25:21 -0700

    Changed names

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,20 @@
-/**
- * Created by lij7451 on 5/11/2017.
- */
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.*;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    Button button;
+    Stage window;
+    Scene scene1, scene2, scene3m, scene3f;
+    Button button, male, female, startm, startf;
 
     public static void main(String[] args) {
         launch(args);
@@ -19,18 +22,80 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Title of the Window");
-        button = new Button("Click me");
+        window = primaryStage;
+        window.setTitle("Journey Through Jilu");
 
-        button.setOnAction(e -> {
-            System.out.println("Lambda expressions are awesome!");
-            System.out.println("hey now brown cow");
-        });
+        Label label1 = new Label("Please enter your name.");
 
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button);
-        Scene scene = new Scene(layout, 300, 250);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        //Form
+        final TextField nameInput = new TextField();
+        //String name = nameInput.getText();
+        nameInput.setPromptText("Enter character name");
+        nameInput.getText();
+
+        final Label l = new Label();
+
+        button = new Button("Submit");
+        button.setOnAction( e -> {
+            l.setText("What is your gender, "+nameInput.getText()+"?");
+            window.setScene(scene2);
+        } );
+
+        //Scene 1
+        VBox layout1 = new VBox(10);
+        layout1.setPadding(new Insets(20, 20, 20, 20));
+        layout1.getChildren().addAll(label1, nameInput, button);
+        scene1 = new Scene(layout1, 300, 250);
+
+        //Scene 2
+        male = new Button("Male");
+        female = new Button("Female");
+        male.setOnAction(e -> window.setScene(scene3m));
+        female.setOnAction(e -> window.setScene(scene3f));
+
+        VBox layout2 = new VBox(10);
+        layout2.setPadding(new Insets(20, 20, 20, 20));
+        layout2.getChildren().addAll(l,male,female);
+        scene2 = new Scene(layout2, 300, 250);
+
+        //Scene 3m
+       Label label3m = new Label("Male. This is you.");
+        VBox layout3m = new VBox();
+        layout3m.setPadding(new Insets(20, 20, 20, 20));
+        Image imageP = new Image("maleChar.png",300,370, false, false);
+        ImageView pent = new ImageView();
+        pent.setImage(imageP);
+        pent.setPreserveRatio(true);
+        pent.setFitHeight(300);
+        pent.setFitWidth(370);
+
+        startm = new Button("Continue");
+        startm.setOnAction(e -> window.setScene(scene2));
+        layout3m.getChildren().addAll(label3m,pent,startm);
+        scene3m = new Scene(layout3m, 300, 400);
+
+        //Scene 3f
+        Label label3f = new Label("Female. This is you.");
+        VBox layout3f = new VBox();
+        layout3f.setPadding(new Insets(20, 20, 20, 20));
+        Image imageJ = new Image("femaleChar.png",300,370, false, false);
+        ImageView julia = new ImageView();
+        julia.setImage(imageJ);
+        julia.setPreserveRatio(true);
+        julia.setFitHeight(300);
+        julia.setFitWidth(370);
+
+        startf = new Button("Continue");
+        startf.setOnAction(e -> window.setScene(scene2));
+        layout3f.getChildren().addAll(label3f,julia,startf);
+        scene3f = new Scene(layout3f, 300, 400);
+
+       //Start
+        window.setScene(scene1);
+        window.show();
     }
+
+
 }
+
+
