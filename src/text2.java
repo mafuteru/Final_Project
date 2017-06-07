da4d30e8378ce6c4d495988d7eee55956683d79f
mafuteru <linkpichucheez@gmail.com>
2017-06-07 15:01:59 -0700

    second half of text

---
+++ src/text2.java
@@ -0,0 +1,111 @@
+
+
+
+
+Text text8a = new Text("You’ve finally arrived to free your friends and the rest of the forest "+
+        "\nresidents from the evil tyranny of Warlock X. But how will you get in?");
+
+Text text8ai = new Text("You transform into a mouse, undetectable and innocuous, to run around "+
+        "\nthe castle’s perimeter. Once you have noted and memorized the guards’ schedules for"+
+        "\ntwo nights, you slip into the castle through a crack in the stones.");
+
+Text text8aii = new Text("You rashly transform into a bear and forcibly break the doors open. "+
+        "\nYou foolhardy creature, don’t you know the guards will see you? The last thing you feel"+
+        "\nis the sharp pain of an arrow piercing your heart. You will never see your friends"+
+        "\nagain, and your friends will never be free again.");
+
+Text text8b = new Text("At last, you’re in the castle! Transforming into a cricket, you slip "+
+        "\nthrough two heavy doors right in front of you, intending to snoop around and find your "+
+        "\nfriends from Wutook as fast as you can. However, as soon as you hop inside the room,"+
+        "\nthe doors suddenly slam shut behind you, and you turn around, startled. A man, covered"+
+        "\nhead to toe in an elaborate brocade uniform, crackling with an aura of power, steps"+
+        "\ninto what you now recognize as a throne room. What do you do?");
+
+Text text8bi = new Text("You successfully run far, far away. However, you will never see your "+
+        "\nfriends again, and your friends will never be free again.");
+
+Text text8bii = new Text("You hide behind some curtains. However, the man eerily casts his eyes"+
+        "\naround and sniffs the air. You tremble, terrified. Suddenly, he cackles. \“A shape"+
+        "\nshifter! Well, well, what have we here!\”");
+
+Text text8c = new Text("You have been caught. You are quite justifiably terrified. As the man"+
+        "\napproaches, you feel frozen in your cricket body. His cold eyes are disorientingly"+
+        "\nnfamiliar, and you begin to have flashes of color and sparks of sound burst in your "+
+        "\nmind. Shaking your head, you try to clear the chaos. ");
+
+Text text8d = new Text("He laughs, a crazy, unhinged laugh. \“Is this another challenger? Well, "+
+        "\nchallenger, see how you do against the great Warlock X himself!\” Without warning,"+
+        "\nhe transforms into a dragon (a dragon? How did he even...) and literally hurls a "+
+        "\nfireball your way. What do you do?");
+
+Text text8di = new Text("Foolishly thinking you can overwhelm the Warlock with your sheer"+
+        "\nsize, you transform into a blue whale, the largest mammal on earth. But alas, in"+
+        "\nyour haste you’ve forgotten: whales don’t live on land. The last thing you hear is "+
+        "\nWarlock X’s cruel laughter, echoing across the throne room.");
+
+Text text8dii = new Text("You’re not sure if you can transform into a mystical creature, but"+
+        "\nit’s worth a shot. With the image pictured in your head, wings sprout from your"+
+        "\nbody and scales begin to cover your skin. Your powers have grown stronger.”");
+
+ Text text8e = new Text("Now a fierce dragon in your own right, you turn to face the"+
+        "\nflabbergasted Warlock X, also in dragon form. Purveying the room, you see a huge "+
+        "\nreflecting pool surrounded by picturesque boulders outside, and a magnificent "+
+        "\nthrone before you. Turning back to the fight, you shoot a huge blast of fire "+
+        "\nonto Warlock X. ");
+
+Text text8f = new Text("He jumps back, roaring in pain and astonishment. With a swipe of"+
+        "\nhis spiked tail, he easily knocks you off your feet, and you fall to the ground, "+
+        "\narm twinging with phantom pain. Growling, he waves his hand and murmurs some "+
+        "\nundecipherable words, and you find yourself forced back into human form, panting "+
+        "\nand bleeding profusely.");
+
+Text text8g = new Text("However, when your disguise peels away to reveal your face, to your"+
+        "\nsurprise, the Warlock’s expression turns from amused to one of both shock and horror.\“"+
+        "\n...It’s you,\” he whispers, aghast. \“Y...you were supposed to have DIED!\” He shouts.");
+
+Text text8gi = new Text("Don’t you know the limits of your strength? Wearing yourself out even "+
+        "\nmore, you die a painful death.");
+
+Text text8gii = new Text("This encounter won’t end like the last. With your last bit of "+
+        "\nstrength, you transform into a tiger, roaring and slamming Warlock X against the wall."+
+         "\n\“Who are you? How do you know me? What are you trying to say?\” you demand.");
+
+Text text9a = new Text("Warlock X’s eyes bulge as he chokes, \“You good for nothing...I always "+
+        "\nknew you would bring trouble for Mother and me the second we entered this room fifteen"+
+        "\nyears ago.\” Startled, you stammer, \“Do you mean to say...I’m your...you’re my--\”");
+
+Text text9b = new Text("\“Stepbrother?\” he asks, a cruel smile curling around his features. "+
+        "\n\“Yes, I am. And Mother and I would have ruled this land, if not for you and your "+
+        "\nidiot of a Father. As if he could ever be a true king, that soft weakling…\” You "+
+        "\ngape at him, totally taken aback. This means you, are a royal! Your father "+
+        "\nwas the king! But if so…");
+
+Text text9c = new Text("\“Where is Father now?\” you demand. He laughs maniacally. \“Dead, as"+
+        "\nyou were supposed to be!\” he screams. \“After Mother died, I knew I had to live"+
+        "\nout her dream by becoming King. You and your father would have stopped me, so I "+
+        "\nkilled him. I thought I killed you too, but it looks like you somehow survived!\” "+
+        "\nAs you hear this horrifying story, you feel the whispers in your head grow stronger,"+
+        "\nyour memories threatening to return. Suddenly, you hit your head hard as you're thrown"+
+        "\nagainst the wall by Warlock X, who has taken advantage of your momentary distraction.");
+
+Text text9d = new Text("\“Now it is time to end you!\” he crows. But you barely hear him as a"+
+        "\nrush of sensory information floods back into your head. As your memories returns,"+
+        "\nyou remember the attack, the terror, and the agony before you blacked out. You "+
+        "\nstruggle back to your feet and roar, \“I prevailed because I am the rightful ruler"+
+        "\nof Jilu, and I survived because I am more powerful than you, the usurper! I have "+
+        "\nfaced hatred and love, challenges and rewards, and you will never take the justice"+
+        "\nfrom this forest! Cower before the power of the rightful ruler, Warlock X!\” ");
+
+Text text9e = new Text("You slam him hard against the windows, so hard he crashes through"+
+        "\nthem. He screams and is knocked unconscious.You transform back into human form and"+
+        "\ncall for the palace guards, who look stunned but overjoyed to see their rightful "+
+        "\nleader back. \“Lock this creature up!\” you command. \“His reign of terror ends here.\”");
+
+Text text10a = new Text("With Warlock X successfully defeated, you run to save your friends."+
+        "\nAs they thank you profusely, even Adelaide hugging you in a rare display of motion,"+
+        "\nyou feel happy and content. You have saved your people, the people who truly "+
+        "\naccepted you and called you their family, and you feel accomplished. In time, the"+
+        "\nForest of Jilu will need a leader once more, and you will be there to forge those "+
+        "\nconnections and rule the land, happily ever after." +
+        "\n**********THE END**********");
+
